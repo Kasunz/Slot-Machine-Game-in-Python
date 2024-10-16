e7661501bcf201fbe44a96ee00a66d094efcbd39
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-16 10:13:06 +0530

    Function to get bet amount and find the total bet

--- game.py
+++ game.py
@@ -1,5 +1,7 @@
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -17,7 +19,7 @@ def deposit():
 
 def get_number_of_lines():
     while True:
-        lines = input("Enter the number of lines to bet on (1-"+str(MAX_LINES)+")")
+        lines = input("Enter the number of lines to bet on (1-"+str(MAX_LINES)+"): ")
         # lines = input(f"Enter the number of lines to bet on (1-{MAX_LINES})")
         if lines.isdigit():
             lines = int(lines)
@@ -30,10 +32,28 @@ def get_number_of_lines():
 
     return lines
 
+def get_bet():
+    while True:
+        bet_amount = input("What would you like to bet on each line? $")
+        if bet_amount.isdigit():
+            bet_amount = int(bet_amount)
+            if MIN_BET <= bet_amount <= MAX_BET:
+                break
+            else:
+                print(f"amount must be between ${MIN_BET} - ${MAX_BET}")
+        else:
+            print("Please enter a number")
+
+    return bet_amount
+
 
 def main():
     balance = deposit()
     lines = get_number_of_lines()
+    bet = get_bet()
+
+    total_bet = bet * lines
+    print(f"You are betting ${bet} on {lines} lines. Total bet: ${total_bet} ")
 
 
 main()
