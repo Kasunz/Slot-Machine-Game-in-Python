0a633a7da25783f329aee461a22d7befe9e999a4
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-16 09:43:56 +0530

    Function to deposit

---
+++ game.py
@@ -0,0 +1,18 @@
+
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $ ")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than zero.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+
+
+# https://youtu.be/th4OBktqK1I
